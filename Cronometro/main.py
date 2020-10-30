9b66978864cb527b373f4071b6541457da67c948
juan75555 <72112435+juan75555@users.noreply.github.com>
2020-10-30 10:34:10 +0100

    Add files via upload

---
+++ Cronometro/main.py
@@ -0,0 +1,37 @@
+import time
+
+print("Hola, esto es un contador.")
+for h in range(0,24):
+    for m in range(0,60):
+        for s in range(0,60):
+            print(h,':',m,':',s)
+            time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
